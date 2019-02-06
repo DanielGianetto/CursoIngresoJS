02b9f22a28fb154284c3ea5b4a4baafd35cd5ec0
Daniel <daniel_gianetto@hotmail.com>
2019-02-06 10:48:24 -0300

    Ejercicio 6, InstrucciónIF hechogit add .

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -2,5 +2,13 @@ function mostrar()
 {
 //tomo la edad  
 
+var edad1;
+
+edad1 = document.getElementById('edad').value;
+
+if(edad1 < 13 || edad1 > 17)
+{
+	alert("No es adolescente");
+}
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -2,7 +2,22 @@ function mostrar()
 {
 //tomo la edad  
 
+var edad1;
 
+edad1 = document.getElementById('edad').value;
+
+if(edad1 >= 18)
+{
+	alert("Es mayor de edad");
+}
+else if(edad1 >= 13 && edad1 <= 17)
+{
+	alert("Es adolescente");
+}
+else(edad1 < 13)
+{
+	alert("Es un niño");
+}
 
 
 }//FIN DE LA FUNCIÓN
