c0a11e4fad64ea87767f04e26a12808215e92e23
DanielGianetto <47353454+DanielGianetto@users.noreply.github.com>
2019-02-06 06:29:02 -0300

    1er ejercicio "unidad 2"
    ejercicio 1 hecho.

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -1,6 +1,25 @@
 function mostrar()
 {
 //tomo la edad  
+	var edad1;
+	//var edad2;
+
+	edad1 = edad.value;
+
+	//edad1 = parseInt(edad1); <no>
+	//edad2 = parseInt(edad1); <no>
+
+	//parseInt(edad1) = edad1; <no funciona>
+
+ 					//para poner que "x" = "y"
+	if(edad1 == 15)	//se escribe "x == y" (con dos signos de igual en lugar de uno cuando se trata de comparar variables, numero, etc).
+	{
+		alert("niña bonita");
+	}
+	else
+	{
+		alert(":)");
+	}
 
 
 
