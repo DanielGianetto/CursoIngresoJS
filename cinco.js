6451876f4a46dc1ee97fad422351609c1cc581cb
Daniel Gianetto <47353454+DanielGianetto@users.noreply.github.com>
2019-02-20 06:05:10 -0300

    Ejercicios 5 y 6 parcial.

---
+++ cinco.js
@@ -0,0 +1,29 @@
+function mostrar()
+{
+
+	var planetas;
+	var mensaje;
+
+	planetas = prompt("Ingresar planeta: ");
+
+	switch(planetas)
+	{
+		case "mercurio":
+		case "venus":
+			mensaje = "Aca hace más calor.";
+			break;
+		case "tierra":
+			mensaje = "Aca vivimos nosotros.";
+			break;
+		case "jupiter":
+		case "saturno":
+		case "urano":
+		case "neptuno":
+			mensaje = "Aca hace más frio.";
+			break;
+		default:
+			mensaje = "Planeta enano o no valido.";
+			break;
+	}
+	alert(mensaje);
+}
