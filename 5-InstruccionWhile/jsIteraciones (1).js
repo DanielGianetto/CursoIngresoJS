1682bd65a48bb62699c21023518ce93b8509e130
Daniel Gianetto <daniel_gianetto@hotmail.com>
2019-02-20 12:19:06 -0300

    ejercicio 2 while

--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -3,16 +3,16 @@ function mostrar()
 //Toda iteración tiene un comienzo, un recorrido y una accion
 //que marca su final.
 
-var contador;
+	var contador;
 
-contador = 0;
+	contador = 0;
 
-while(contador < 10)
-{
+	while(contador < 10)
+	{
 
-	contador = contador + 1;
-	console.log(contador);
-}
+		contador = contador + 1;
+		console.log(contador);
+	}
 
 
 
--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,16 @@
 function mostrar()
 {
 
-var clave = prompt("ingrese el número clave.");
+var clave;
+
+clave = prompt("ingrese el número clave.");
+clave = "utn750";
+
+while(clave != "utn750")
+{
+	clave = prompt("ingrese el número clave.");
+}
+
 
 
 }//FIN DE LA FUNCIÓN
